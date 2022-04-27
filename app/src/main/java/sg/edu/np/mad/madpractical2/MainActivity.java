e97529d42635e5fdf5f9155bddce5311d862e4b7
NotAddison <S10222525@connect.np.edu.sg>
2022-04-27 15:53:15 +0800

    Update MainActivity.java : - Added main follow follow button functionality.

--- app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
@@ -3,6 +3,8 @@ package sg.edu.np.mad.madpractical2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        User user1 = new User("John", "Hairy Human", 1, false);
+        User user1 = new User("Addison", "Y2 IT Student", 1, false);
+
+        // Get button
+        Button followButton = findViewById(R.id.followButton);
+
+        // Check if user is following
+        if (user1.followed){
+            followButton.setText("Unfollow");
+        }
+
+        // Set onClickListener for Follow Button to update text
+        followButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user1.followed){
+                    user1.followed = false;
+                    followButton.setText("Follow");
+                } else {
+                    user1.followed = true;
+                    followButton.setText("Unfollow");
+                }
+            }
+        });
     }
-
-
-
 }
